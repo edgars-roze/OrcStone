d45d1d4c4bdf6fd26d8fadc7c546555ae716af72
edgars-roze <klopojed.son@gmail.com>
2025-02-02 16:14:26 +0200

    Add music

--- Game.cs
+++ Game.cs
@@ -1,22 +1,24 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Main;
 
 public class Game : Microsoft.Xna.Framework.Game
 {
-    private Texture2D _grassTexture;
     private Texture2D _orcTexture;
-    private GraphicsDeviceManager _graphics;
+    private readonly GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     
     public Game()
     {
         _graphics = new GraphicsDeviceManager(this);
+        var song = Content.Load<Song>("Content\\Music");
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+        
+        MediaPlayer.Play(song);
     }
 
     protected override void Initialize()
@@ -36,8 +38,7 @@ public class Game : Microsoft.Xna.Framework.Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
-        
-        _grassTexture = Content.Load<Texture2D>("grass");
+
         _orcTexture = Content.Load<Texture2D>("orc");
     }
 
@@ -57,7 +58,7 @@ public class Game : Microsoft.Xna.Framework.Game
         GraphicsDevice.Clear(new Color(86, 50, 38));
 
         // TODO: Add your drawing code here
-
+        
         _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
         
         _spriteBatch.Draw(_orcTexture, new Vector2(160, 160), null, Color.White, 0f, Vector2.Zero, 10f, 0, 0);
